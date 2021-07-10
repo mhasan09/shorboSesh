774b0bffd5bd49faabe649cc71f7490e51646c6c
Mahabubul Hasan <shourav6688@gmail.com>
2021-07-10 17:01:59 +0600

    feat:refresh after per API call

--- shorboSeshApp/urls.py
+++ shorboSeshApp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.sorboSesh, name='home'),
     path('api/news', views.news_api_view.as_view(), name='news_api'),
     path('api/news/<int:pk>', views.news_api_detail_view.as_view(), name='new_api_detail'),
 
--- shorboSeshApp/views.py
+++ shorboSeshApp/views.py
@@ -4,24 +4,14 @@ from rest_framework import generics
 from .models import NEWS
 from requests_html import HTMLSession
 from shorboSeshApp.serializers import NEWS_SERIALIZERS
-def sorboSesh(requests):
+
+class news_api_view(generics.ListCreateAPIView):
+    NEWS.objects.all().delete()
     session = HTMLSession()
     site = 'https://www.prothomalo.com/collection/latest'
     html = session.get(site)
     soup = BeautifulSoup(html.content, "html.parser")
     connection = soup.find_all("div", {"class": "customStoryCard9-m__story-data__2qgWb"}, "href")
-
-    # for i in connection:
-    #     temp_dict = dict()
-    #     temp_dict['NEWS_TITLE'] = i.h2.text
-    #     try:
-    #         temp_dict['NEWS_SUBTITLE'] = i.span.text
-    #     except Exception as e:
-    #         pass
-    #     temp_dict['NEWS_LINK'] = i.find('a')['href']
-    #     temp_dict['NEWS_SOURCE'] = 'Promothom Alo'
-    #     temp_dict['POST_CREATED_AT'] = i.time.text
-
     for i in connection:
         obj = NEWS()
         obj.NEWS_TITLE = i.h2.text
@@ -33,8 +23,6 @@ def sorboSesh(requests):
         obj.NEWS_SOURCE = 'Prothom Alo'
         obj.POST_CREATED_AT = i.time.text
         obj.save()
-
-class news_api_view(generics.ListCreateAPIView):
     queryset = NEWS.objects.all()
     serializer_class = NEWS_SERIALIZERS
 
